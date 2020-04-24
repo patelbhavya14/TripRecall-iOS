7982a05647cf896072d3ac4498d73595157dfcfa
Bhavya Patel <patel.bhavya@husky.neu.edu>
2020-04-23 21:32:44 -0400

    Attraction add, update

---
+++ TripRecall/Custom View/AttractionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AttractionTableViewCell.swift
+//  TripRecall
+//
+//  Created by Bhavya Patel on 21/04/20.
+//  Copyright © 2020 teXoftgen. All rights reserved.
+//
+
+import UIKit
+
+class AttractionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TripRecall/Trip/CalenderViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CalenderViewController.swift
+//  TripRecall
+//
+//  Created by Bhavya Patel on 23/04/20.
+//  Copyright © 2020 teXoftgen. All rights reserved.
+//
+
+import UIKit
+
+class CalenderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
